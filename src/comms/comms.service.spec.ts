7b405ab78ecfedd0233d129a381c7642726fdc15
Alex Lopez <76691426+lopezelpesado@users.noreply.github.com>
2024-10-09 15:35:21 +0100

    add comms service integration test

--- src/comms/comms.service.spec.ts
+++ src/comms/comms.service.spec.ts
@@ -10,9 +10,53 @@ describe('CommsService', () => {
     }).compile();
 
     service = module.get<CommsService>(CommsService);
+
+    service['users'] = [
+      {
+        id: 'ff535484-6880-4653-b06e-89983ecf4ed5',
+        firstName: 'Kayleigh',
+        lastName: 'Wilderman',
+        email: 'Kayleigh_Wilderman@hotmail.com',
+        cats: [
+          {
+            name: 'Dorian',
+            subscriptionActive: true,
+            breed: 'Thai',
+            pouchSize: 'C',
+          },
+          {
+            name: 'Ocie',
+            subscriptionActive: true,
+            breed: 'Somali',
+            pouchSize: 'F',
+          },
+          {
+            name: 'Eldridge',
+            subscriptionActive: false,
+            breed: 'Himalayan',
+            pouchSize: 'A',
+          },
+        ],
+      },
+    ];
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return the correct delivery message and details for the user', () => {
+    const userId = 'ff535484-6880-4653-b06e-89983ecf4ed5';
+    const expectedResponse = {
+      title: 'Your next delivery for Dorian and Ocie',
+      message:
+        "Hey Kayleigh! In two days' time, we'll be charging you for your next order for Dorian and Ocie's fresh food.",
+      totalPrice: 134,
+      freeGift: true,
+    };
+
+    const result = service.getNextDelivery(userId);
+
+    expect(result).toEqual(expectedResponse);
   });
 });
