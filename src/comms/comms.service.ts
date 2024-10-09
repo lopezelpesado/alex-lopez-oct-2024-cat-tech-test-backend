25921e5b3a335108878055129b572a7353d29c75
Alex Lopez <76691426+lopezelpesado@users.noreply.github.com>
2024-10-09 15:23:07 +0100

    add user lookup

--- src/comms/comms.service.ts
+++ src/comms/comms.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
+type PouchSize = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+
+interface Cat {
+  name: string;
+  subscriptionActive: boolean;
+  breed: string;
+  pouchSize: PouchSize;
+}
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  cats: Cat[];
+}
 
 @Injectable()
 export class CommsService {
+  private users: User[] = [];
+
+  constructor() {
+    this.loadUsers();
+  }
+
+  private async loadUsers() {
+    const filePath = path.join(__dirname, '..', 'data', '../../data.json');
+    const jsonData = await fs.readFile(filePath, 'utf-8');
+    this.users = JSON.parse(jsonData);
+  }
+
+  getUserById(UserId: string) {
+    const user = this.users.find((user) => user.id === UserId);
+
+    return user;
+  }
+
   getNextDelivery(id: string) {
-    console.log({ id });
+    console.log('user: ', this.getUserById(id));
 
     return {
       title: 'Your next delivery for Dorian and Ocie',
